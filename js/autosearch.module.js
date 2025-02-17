44a0312b17d9b24d00995908937c53bf6eb05a1b
lavinse100 <119337552+lavinse100@users.noreply.github.com>
2025-02-17 15:55:22 -0500

    small bug fix
    changing scope to one without locations, and then back to one with locations, made the autosearch stop working for location changes

--- js/autosearch.module.js
+++ js/autosearch.module.js
@@ -1,22 +1,31 @@
 // component to activate search when search scope changed updated 09/11/17 DW
+// lightly edited for BC 2/19/25 EL
 angular
   .module('autosearch',[])
   .component('prmTabsAndScopesSelectorAfter',{
     bindings: {parentCtrl: '<'},
-    controller: function($scope){
+    controller: function(){
       setTimeout(function() {
-        function activateSearch() {
-          setTimeout(function() {
-             document.getElementsByClassName("zero-margin button-confirm md-button md-primoExplore-theme")[0].click()
-          }, 500)
-        }
+        getSearchScopes();
 
-        var searchScopes = document.querySelectorAll('[id^="select_option_"]')
-        for (var i in searchScopes) {
-          searchScopes[i].onclick = function() {
-            activateSearch();
-          };
+        function activateSearch() {  // execute clicking search button after 500 millisecs
+          setTimeout(function() {
+             document.getElementsByClassName("zero-margin button-confirm md-button md-primoExplore-theme")[0].click();
+          }, 500);
+          setTimeout(function() {  // get search scopes
+            getSearchScopes();
+         }, 500)
         }
-      }, 500)
+        
+        function getSearchScopes() {
+          var searchScopes = document.querySelectorAll('[id^="select_option_"]'); // contain attribute id that starts w "select_option_"
+          for (var i in searchScopes) {
+            console.log(searchScopes[i]);
+            searchScopes[i].onclick = function() {
+              activateSearch();
+            };
+          }
+        }
+      }, 500);
     }
  });
